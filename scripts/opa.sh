f6bc0bbf4f8621f8312630d0f71e629702735f1c
Kjartan Müller <kjartan@muller.no>
2024-07-05 15:01:36 +0200

    Update opa.sh

--- scripts/opa.sh
+++ scripts/opa.sh
@@ -1,4 +1,6 @@
-# touch "$(pwd)/src/opa/bundle.tar.gz"
-# chmod a+rw "$(pwd)/src/opa/bundle.tar.gz"
+echo "set some file permissions"
+touch "$(pwd)/src/opa/bundle.tar.gz"
+chmod a+rw "$(pwd)/src/opa/bundle.tar.gz"
+echo "run docker"
 docker run -v "$(pwd)/src/opa:/src" openpolicyagent/opa build -t wasm -e example/allow ./src/todo.rego -o ./src/bundle.tar.gz
 tar xfC "$(pwd)/src/opa/bundle.tar.gz" "$(pwd)/src/opa/" "/policy.wasm"
